55cc3a9f842734291c7d487f6b8bf5a7994864c6
acl13 <128934431+acl13@users.noreply.github.com>
2023-07-22 14:22:51 -0400

    create getComputerChoice function

--- rps.js
+++ rps.js
@@ -1,7 +1,28 @@
+
 const input = prompt("Rock, Paper, or Scissors?");
 const playerChoice = input.toLowerCase();
 console.log(playerChoice);
 
+
+
 if (playerChoice != "rock" && playerChoice != "scissors" && playerChoice != "paper") {
     alert("Invalid input");
 }
+
+
+
+const choice = ["rock","paper","scissors"];
+console.log(choice);
+
+let computerChoice
+
+function getComputerChoice(arr) {
+    computerChoice = arr[Math.floor(Math.random() * arr.length)];
+    return computerChoice;
+}
+
+getComputerChoice(choice);
+
+console.log(computerChoice);
+
+
