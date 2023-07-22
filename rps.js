d83698b3a18a03c57db0cfc69d1f984aec4f068d
acl13 <128934431+acl13@users.noreply.github.com>
2023-07-22 15:01:17 -0400

    create playRound function

--- rps.js
+++ rps.js
@@ -3,13 +3,13 @@ const input = prompt("Rock, Paper, or Scissors?");
 const playerChoice = input.toLowerCase();
 console.log(playerChoice);
 
-
+/*
 
 if (playerChoice != "rock" && playerChoice != "scissors" && playerChoice != "paper") {
     alert("Invalid input");
 }
 
-
+*/
 
 const choice = ["rock","paper","scissors"];
 console.log(choice);
@@ -23,6 +23,33 @@ function getComputerChoice(arr) {
 
 getComputerChoice(choice);
 
-console.log(computerChoice);
+console.log(computerChoice); 
+
+
+function playRound(playerChoice, computerChoice){
+    if(playerChoice === computerChoice) {
+        console.log("It's a tie!")
+    }  
+    else if(playerChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats scissors.");
+    }
+    else if(playerChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats rock.")
+    }
+    else if(playerChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats paper.")
+    }
+    else if(playerChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats rock.")
+    }
+    else if(playerChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats paper.")
+    }
+    else if(playerChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats scissors.");
+    }
+}
+
+playRound(playerChoice, computerChoice);
 
 
