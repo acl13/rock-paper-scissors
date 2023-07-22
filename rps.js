79f35a541ecaff4b55b192ff720b2b5358f47131
acl13 <128934431+acl13@users.noreply.github.com>
2023-07-22 13:52:11 -0400

    prompt user input/add invalid input alert

--- rps.js
+++ rps.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const input = prompt("Rock, Paper, or Scissors?");
+const playerChoice = input.toLowerCase();
+console.log(playerChoice);
+
+if (playerChoice != "rock" && playerChoice != "scissors" && playerChoice != "paper") {
+    alert("Invalid input");
+}
